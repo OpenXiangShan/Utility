c83eac5e93a94b514f7aca26f1c58e3934471d3b
Maxpicca-Li <maxpicca@qq.com>
2023-03-19 16:55:58 +0800

    filereg: add write function for unification (#14)

--- src/main/scala/utility/FileRegisters.scala
+++ src/main/scala/utility/FileRegisters.scala
@@ -1,5 +1,7 @@
 package utility
 
+import java.io.{File, FileWriter}
+
 object FileRegisters {
   var files: Seq[(String, () => String)] = Nil
 
@@ -9,6 +11,20 @@ object FileRegisters {
 
   def contains(filename: String): Boolean = {
     files.foldLeft(false)((t, s) => {s._1 == filename | t})
+  }
+
+  def write(fileDir: String = "./build", filePrefix: String = ""): Unit = {
+    files.foreach { case (filename, contents) =>
+      writeOutputFile(fileDir, filePrefix + filename, contents())
+    }
+  }
+
+  def writeOutputFile(targetDir: String, fname: String, contents: String): File = {
+    val f = new File(targetDir, fname)
+    val fw = new FileWriter(f)
+    fw.write(contents)
+    fw.close
+    f
   }
 }
 
