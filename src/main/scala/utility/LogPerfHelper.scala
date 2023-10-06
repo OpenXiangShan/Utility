22518a8f92547d0398f34ef4c9f52115f6032238
Yinan Xu <xuyinan@ict.ac.cn>
2023-10-06 22:28:14 +0800

    Disable Verilator tracing for LogPerfHelper blackbox

--- src/main/scala/utility/LogPerfHelper.scala
+++ src/main/scala/utility/LogPerfHelper.scala
@@ -18,6 +18,8 @@ class LogPerfHelper extends BlackBox with HasBlackBoxInline {
       |  `define SIM_TOP_MODULE_NAME SimTop
       |`endif
       |
+      |/*verilator tracing_off*/
+      |
       |module LogPerfHelper (
       |  output [63:0] timer,
       |  output        logEnable,
