210de5a711d3676fd1bae873a472fe79c63583f1
sumailyyc <810583334@qq.com>
2024-02-23 12:05:05 +0800

    Utility: improve clock-gating coverage

---
+++ src/main/scala/utility/ClockGatedReg.scala
@@ -0,0 +1,28 @@
+/***************************************************************************************
+* Copyright (c) 2020-2021 Institute of Computing Technology, Chinese Academy of Sciences
+* Copyright (c) 2020-2021 Peng Cheng Laboratory
+*
+* XiangShan is licensed under Mulan PSL v2.
+* You can use this software according to the terms and conditions of the Mulan PSL v2.
+* You may obtain a copy of Mulan PSL v2 at:
+*          http://license.coscl.org.cn/MulanPSL2
+*
+* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+*
+* See the Mulan PSL v2 for more details.
+***************************************************************************************/
+
+package utility
+
+import chisel3._
+import chisel3.util._
+
+object GatedValidRegNext {
+  def apply(next: Bool, init: Bool = false.B): Bool = {
+    val last = Wire(next.cloneType)
+    last := RegEnable(next, init, next || last)
+    last
+  }
+}
