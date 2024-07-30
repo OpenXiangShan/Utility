786dd5864d539e1f7387419f3cccc7de7ea736da
Haojin Tang <tanghaojin@outlook.com>
2024-07-30 23:41:31 +0800

    IntBuffer: add depth parameter

--- src/main/scala/utility/IntBuffer.scala
+++ src/main/scala/utility/IntBuffer.scala
@@ -5,21 +5,21 @@ import org.chipsalliance.cde.config.Parameters
 import freechips.rocketchip.diplomacy.{LazyModule, LazyModuleImp}
 import freechips.rocketchip.interrupts.IntAdapterNode
 
-class IntBuffer(implicit p: Parameters) extends LazyModule {
+class IntBuffer(depth: Int = 1)(implicit p: Parameters) extends LazyModule {
 
   val node = IntAdapterNode()
 
   lazy val module = new LazyModuleImp(this){
     for(((in, edgeIn), (out, edgeOut)) <- node.in.zip(node.out)){
-      out := RegNext(in, 0.U.asTypeOf(in))
+      out := RegNextN(in, depth, Some(0.U.asTypeOf(in)))
     }
   }
 
 }
 
 object IntBuffer {
-  def apply()(implicit p: Parameters) = {
-    val intBuffer = LazyModule(new IntBuffer)
+  def apply(depth: Int = 1)(implicit p: Parameters) = {
+    val intBuffer = LazyModule(new IntBuffer(depth))
     intBuffer.node
   }
 }
