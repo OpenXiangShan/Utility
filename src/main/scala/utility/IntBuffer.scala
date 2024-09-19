c54d93a18ff7bf5f09aaf435cef6898fc16e535a
Tang Haojin <tanghaojin@outlook.com>
2024-09-19 15:48:55 +0800

    feat(IntBuffer): add cdc option for synchronizer (#79)

--- src/main/scala/utility/IntBuffer.scala
+++ src/main/scala/utility/IntBuffer.scala
@@ -4,22 +4,26 @@ import chisel3._
 import org.chipsalliance.cde.config.Parameters
 import freechips.rocketchip.diplomacy.{LazyModule, LazyModuleImp}
 import freechips.rocketchip.interrupts.IntAdapterNode
+import freechips.rocketchip.util.AsyncResetSynchronizerShiftReg
 
-class IntBuffer(depth: Int = 1)(implicit p: Parameters) extends LazyModule {
+class IntBuffer(depth: Int = 1, cdc: Boolean = false)(implicit p: Parameters) extends LazyModule {
 
   val node = IntAdapterNode()
 
   lazy val module = new LazyModuleImp(this){
-    for(((in, edgeIn), (out, edgeOut)) <- node.in.zip(node.out)){
-      out := RegNextN(in, depth, Some(0.U.asTypeOf(in)))
+    for(((in, edgeIn), (out, edgeOut)) <- node.in.zip(node.out)) {
+      out := (cdc match {
+        case true =>  AsyncResetSynchronizerShiftReg(in, depth, 0)
+        case false => RegNextN(in, depth, Some(0.U.asTypeOf(in)))
+      })
     }
   }
 
 }
 
 object IntBuffer {
-  def apply(depth: Int = 1)(implicit p: Parameters) = {
-    val intBuffer = LazyModule(new IntBuffer(depth))
+  def apply(depth: Int = 1, cdc: Boolean = false)(implicit p: Parameters) = {
+    val intBuffer = LazyModule(new IntBuffer(depth, cdc))
     intBuffer.node
   }
 }
