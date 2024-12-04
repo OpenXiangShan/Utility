5e51b63ead2c659059cc11d21073114ca4db3226
klin02 <youkunlin20@mails.ucas.ac.cn>
2024-12-04 14:17:36 +0800

    feat(LogPerf): collect XSLogs to SimTop.LogPerfEndpoint
    XSLog depends on LogPerfCtrl declared at Top Module. Previous we
    annotate such signal as dontTouch, and accessed through Hierarchical
    name like SimTop.xx by dummy LogPerfHelper.
    
    However, as XSLog is called in many spaces in DUT, which are not
    visible to each other, especailly some in WhenContext. XS will generate
    thousands of LogPerfHelper to get same LogPerfCtrl. Too many module
    instantiations greately slow down compilation speed, especailly in
    Palladium (more than 5 times slower than same DUT without Log).
    
    This change collect all XSLog to SimTop.LogPerfEndpoint, with
    LogPerfCtrl directly passed by IO. Some tips as follows:
    
    1. Not call XSLog inside whenContext. To collect XSLogs, we should
    access Cond and Data from other module, but data in WhenContext is
    not accessible even through tap. Use XSLog(cond, pable) instead of
    when(cond) {XSLog(pable)}. We also add chisel Internal API currentWhen
    to check that.
    
    2. Generate Hierarchical Module path through FIRRTL transforms.
    Sometimes we want to append module path for better debugging.
    XSCompatibility add a hacky way to use Chisel internal API to get
    tag of current Module. Then we will replace these tag with
    path during ChiselStage. Note path can only be acessed after circuit
    elaboration.
    
    3. Register and invoke caller of XSPerf and related object. As XSPerf
    depends on LogPerfCtrl such as dump. We should deferred apply() until
    collect. So we regirster collect() method when firstly apply XSLog,
    then XSLog will automatically call XSPerf.collect() method during
    collection. Note deferred apply is called in another module, so original
    module tag should be recorded for path generation.
    
    4. Concat XSLogs with same condition. Too many fwrites in same module
     will cause UPOPTTHREADS warning with 16-threads Verilator. Consider
     many XSLogs have same condition (especailly XSPerfs), we reuse same
     condition and concat their printables to reduce fwrites. Note we also
     limit size of concatation to 1000 to avoid segmentation fault caused
     by too long printf.

--- src/main/scala/utility/LogUtils.scala
+++ src/main/scala/utility/LogUtils.scala
@@ -17,8 +17,13 @@
 package utility
 
 import chisel3._
+import chisel3.reflect.DataMirror.isVisible
+import chisel3.util.experimental.BoringUtils.tapAndRead
+import chisel3.experimental.BaseModule
 import org.chipsalliance.cde.config.{Field, Parameters}
 import XSLogLevel.XSLogLevel
+
+import scala.collection.mutable.ListBuffer
 
 case object LogUtilsOptionsKey extends Field[LogUtilsOptions]
 case class LogUtilsOptions
@@ -40,31 +45,121 @@ object XSLogLevel extends Enumeration {
   val OFF   = Value("OFF  ")
 }
 
+// Correspond to XSLog apply
+case class LogPerfParam (
+  debugLevel: XSLogLevel,
+  prefix: Boolean,
+  cond: Bool,
+  fmt: String,
+  data: Seq[Data],
+  moduleTag: String
+)
+
+class LogPerfIO extends Bundle {
+  val timer = UInt(64.W)
+  val logEnable = Bool()
+  val clean = Bool()
+  val dump = Bool()
+}
+
 object XSLog {
-  val MagicStr = "__PERCENTAGE_M__"
-  def apply(debugLevel: XSLogLevel, ctrlInfoOpt: Option[LogPerfIO] = None)
-           (prefix: Boolean, cond: Bool, pable: Printable)(implicit p: Parameters): Unit =
-  {
+  private val logInfos = ListBuffer.empty[LogPerfParam]
+  private val callBacks = ListBuffer.empty[(LogPerfIO) => Unit]
+  private val logModules = ListBuffer.empty[BaseModule]
+
+  private def unpackPrintable(pable: Printable): (String, Seq[Data]) = {
+    val fmt = ListBuffer.empty[String]
+    val data = ListBuffer.empty[Data]
+    pable match {
+      case Printables(p) => p.foreach { pb =>
+        val (subfmt, subdata) = unpackPrintable(pb)
+        fmt += subfmt
+        data ++= subdata
+      }
+      case y: FirrtlFormat => {
+        data += y.bits
+        y match {
+          case Decimal(d) => fmt += "%d"
+          case Hexadecimal(x) => fmt += "%x"
+          case Binary(b) => fmt += "%b"
+          case Character(c) => fmt += "%c"
+        }
+      }
+      case PString(str) => fmt += str
+      case other => throw new IllegalArgumentException("XSLog not support unpack: " + other.toString)
+    }
+    (fmt.mkString(""), data.toSeq)
+  }
+
+  // Access source Data in or not in current Module, called in LogPerfEndpoint to wrap boring wires
+  def tapInfos: Seq[LogPerfParam] = {
+    def tapOrGet[T <: Data](data: T): T = {
+      if (isVisible(data)) data else tapAndRead(data)
+    }
+
+    logInfos.toSeq.map { info =>
+      info.copy(
+        cond = tapOrGet(info.cond),
+        data = info.data.map(d => tapOrGet(d))
+      )
+    }
+  }
+
+  // XSPerf depends on passed LogPerfIO, so they deferred apply to endpoint
+  // so original module should be passed for hierarchical module name
+  def apply(debugLevel: XSLogLevel, curModOpt: Option[BaseModule])
+           (prefix: Boolean, cond: Bool, pable: Printable)(implicit p: Parameters): Unit = {
     val logOpts = p(LogUtilsOptionsKey)
     val enableDebug = logOpts.enableDebug && debugLevel != XSLogLevel.PERF
     val enablePerf = logOpts.enablePerf && debugLevel == XSLogLevel.PERF
     if (!logOpts.fpgaPlatform && (enableDebug || enablePerf || debugLevel == XSLogLevel.ERROR)) {
-      val ctrlInfo = ctrlInfoOpt.getOrElse(Module(new LogPerfHelper).io)
-      val logEnable = ctrlInfo.logEnable
-      val logTimestamp = ctrlInfo.timer
-      val check_cond = (if (debugLevel == XSLogLevel.ERROR) true.B else logEnable) && cond
-      when (check_cond) {
-        val commonInfo = p"[$debugLevel][time=$logTimestamp] $MagicStr: "
-        printf((if (prefix) commonInfo else p"") + pable)
-        if (debugLevel >= XSLogLevel.ERROR) {
-          assert(false.B)
+      require(chisel3.XSCompatibility.currentWhen.isEmpty,
+        "XSLog to be collect not supported inside whenContext, use XSLog(cond, pable) instead")
+      require(chisel3.XSCompatibility.currentModule.isDefined, "XSLog should be called inside a module")
+      val curMod = curModOpt.getOrElse(chisel3.XSCompatibility.currentModule.get)
+      if (!logModules.contains(curMod)) logModules += curMod
+      val (fmt, data) = unpackPrintable(pable)
+      val moduleTag = curMod.toString
+      // Deferred and buffered apply()
+      if (debugLevel == XSLogLevel.ERROR) {
+        when(cond) {
+          assert(false.B) //assert at current module for better error location
         }
       }
+      logInfos.addOne(LogPerfParam(debugLevel, prefix, cond, fmt, data, moduleTag))
     }
   }
-  def apply(debugLevel: XSLogLevel, ctrlInfo: LogPerfIO)
+
+  def apply(debugLevel: XSLogLevel)
            (prefix: Boolean, cond: Bool, pable: Printable)(implicit p: Parameters): Unit = {
-    apply(debugLevel, Some(ctrlInfo))(prefix, cond, pable)
+    apply(debugLevel, None)(prefix, cond, pable)
+  }
+
+  def collect(ctrl: LogPerfIO)(implicit p: Parameters): Unit = {
+    val logEndpoint = Module(new LogPerfEndpoint())
+    logEndpoint.io := ctrl
+  }
+
+  def collect(timer: UInt, logEnable: Bool, clean: Bool, dump: Bool)(implicit p: Parameters): Unit = {
+    val ctrl = Wire(new LogPerfIO)
+    ctrl.timer := timer
+    ctrl.logEnable := logEnable
+    ctrl.clean := clean
+    ctrl.dump := dump
+    collect(ctrl)
+  }
+
+  // As XSPerf depends on LogPerfIO, their apply will be buffered until collection
+  // Register collect() method from Callers when first apply, then call that during collection
+  def registerCaller(caller: LogPerfIO => Unit): Unit = callBacks += caller
+  def invokeCaller(ctrl: LogPerfIO): Unit = callBacks.foreach(caller => caller(ctrl))
+
+  // Should only be called during firrtl phase(ChiselStage)
+  // PathName can not be accessed until circuit elaboration
+  def replaceFIRStr(str: String): String = {
+    logModules.foldLeft(str) { case (acc, mod) =>
+      acc.replace(mod.toString, mod.pathName)
+    }
   }
 }
 
@@ -90,3 +185,36 @@ object XSInfo extends LogHelper(XSLogLevel.INFO)
 object XSWarn extends LogHelper(XSLogLevel.WARN)
 
 object XSError extends LogHelper(XSLogLevel.ERROR)
+
+private class LogPerfEndpoint()(implicit p: Parameters) extends Module {
+  val io = IO(Input(new LogPerfIO))
+  def concatAndPrint(infos: Seq[LogPerfParam]): Unit = {
+    infos.grouped(1000).foreach { infos =>
+      val pable = infos.map { info =>
+        val commonInfo = p"[${info.debugLevel}][time=${io.timer}] ${info.moduleTag}: "
+        (if (info.prefix) commonInfo else p"") + Printable.pack(info.fmt, info.data: _*)
+      }.reduce(_ + _)
+      printf(pable)
+    }
+  }
+
+  // To collect deferred call from XSPerf/..., invoke all registered caller
+  XSLog.invokeCaller(io)
+  // Group printfs with same cond to reduce system tasks for better thread schedule
+  XSLog.tapInfos.groupBy(_.cond).values.foreach{ infos =>
+    val cond = infos.head.cond
+    val errs = infos.filter(_.debugLevel == XSLogLevel.ERROR)
+    if (!errs.isEmpty) {
+      when (cond) {
+        concatAndPrint(errs)
+        // assert at local module for better error location
+      }
+    }
+    val logs = infos.filterNot(_.debugLevel == XSLogLevel.ERROR)
+    if (!logs.isEmpty) {
+      when(io.logEnable && cond) {
+        concatAndPrint(logs)
+      }
+    }
+  }
+}
