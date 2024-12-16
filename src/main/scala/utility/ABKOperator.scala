d425857fc45fa5215a385bb47ac67c2817b491cd
zhanglinjuan <zhanglinjuan16@mails.ucas.ac.cn>
2024-12-16 20:28:37 +0800

    feat(ABKOperator): a faster way to compute `A + B === K`

---
+++ src/main/scala/utility/ABKOperator.scala
@@ -0,0 +1,38 @@
+/***************************************************************************************
+* Copyright (c) 2020-2021 Institute of Computing Technology, Chinese Academy of Sciences
+* Copyright (c) 2020-2021 Peng Cheng Laboratory
+*
+* XiangShan is licensed under Mulan PSL v2.
+* You can use this software according to the terms and conditions of the Mulan PSL v2.
+* You may obtain a copy of Mulan PSL v2 at:
+*          http://license.coscl.org.cn/MulanPSL2
+*
+* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+*
+* See the Mulan PSL v2 for more details.
+***************************************************************************************/
+
+package utility
+
+import chisel3._
+import chisel3.util._
+
+/**
+  * K = A + B Comparator
+  * 
+  * This utility is used to determine if (A + B = K). This comparison can be done faster than computing A + B because
+  * no carry propagation is necessary.
+  */
+object ABKOperator {
+  def apply(a: UInt, b: UInt, k: UInt): Bool = {
+    val width = a.getWidth
+    require(b.getWidth === width && k.getWidth === width)
+    require(width > 0)
+    val carryRequire = a ^ b ^ k
+    val carryProduce = (a & b) | (a ^ b) & ~k
+    val equal = !carryRequire(0).asBool && carryProduce.take(width - 1) === (carryRequire >> 1)
+    equal
+  }
+}
