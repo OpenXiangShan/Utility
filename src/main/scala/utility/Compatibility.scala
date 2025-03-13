8b83e30910d421f7add5185fc0a0f2128540c3f6
ambulGruidae <xr.zhao@student.cup.edu.cn>
2025-03-13 16:00:31 +0800

    Remove deprecated `currentWhen` usage in chisel7
    Signed-off-by: ambulGruidae <xr.zhao@student.cup.edu.cn>

--- src/main/scala/utility/Compatibility.scala
+++ src/main/scala/utility/Compatibility.scala
@@ -2,12 +2,13 @@ package chisel3 {
 
   import chisel3.internal._
   import chisel3.experimental.BaseModule
+  import scala.collection.mutable
 
   object XSCompatibility {
     // Return the internal implicit BaseModule
     def currentModule: Option[BaseModule] = Builder.currentModule
 
     // Return the internal implicit whenContext
-    def currentWhen: Option[WhenContext] = Builder.currentWhen
+    def currentWhen: Option[WhenContext] = Option.when(mutable.Stack.empty[WhenContext].nonEmpty)(mutable.Stack.empty[WhenContext].top)
   }
 }
