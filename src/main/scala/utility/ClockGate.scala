fe2f24fbce742495dfc86c1b319acdb616245df7
fengkehan <fengkehan@bosc.ac.cn>
2025-07-24 15:02:11 +0800

    fix: Add a clock gate for synthesis

--- src/main/scala/utility/ClockGate.scala
+++ src/main/scala/utility/ClockGate.scala
@@ -37,12 +37,16 @@ class ClockGate extends BlackBox with HasBlackBoxInline {
       |  input  wire CK,
       |  output wire Q
       |);
+      |`ifdef SYNTHESIS
+      |  assign Q = CK;
+      |`else
       |  reg EN;
       |  always_latch begin
       |    if(!CK) EN = TE | E;
       |  end
       |  assign Q = CK & EN;
       |endmodule
+      |`endif
       |
       |""".stripMargin
   setInline("ClockGate.sv", sverilog)
