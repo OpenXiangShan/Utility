22e2d8dc3be941ddd336247ef8c037a279cefb79
zhang yukun <zhangyukun223@mails.ucas.ac.cn>
2026-09-04 15:23:43 +0800

    feat(MemReqSource): add matrix request sources (#158)
    * feat(MemReqSource): add matrix request sources
    
    * fix(MemReqSource): keep matrix traffic out of CPU requests
    
    ---------
    
    Co-authored-by: zhang yukun <zhangyukun@node020.bosccluster.com>

--- src/main/scala/utility/TLUtils/BusKeyField.scala
+++ src/main/scala/utility/TLUtils/BusKeyField.scala
@@ -45,6 +45,8 @@ object MemReqSource extends Enumeration {
   val Prefetch2L3Stride = Value("Prefetch2L3Stride")
   val Prefetch2L3Berti = Value("Prefetch2L3Berti")
   val Prefetch2L3Unknown = Value("Prefetch2L3Unknown")
+  val CPUMatrixData = Value("CPUMatrixData")
+  val Prefetch2L2Matrix = Value("Prefetch2L2Matrix")
   val ReqSourceCount = Value("ReqSourceCount")
 
   val reqSourceBits = log2Ceil(ReqSourceCount.id)
@@ -70,6 +72,7 @@ object MemReqSource extends Enumeration {
     reqSource === Prefetch2L2Berti.id.U ||
     reqSource === Prefetch2L2NL.id.U ||
     reqSource === Prefetch2L2CDP.id.U ||
+    reqSource === Prefetch2L2Matrix.id.U ||
     reqSource === Prefetch2L2Unknown.id.U
   }
 }
